fdea45176d2f28daf06ef000a872cebfc4c569d4
Evan Beck <evandrydenbeck@gmail.com>
2022-03-10 10:56:04 -0500

    reworked db/filter names options

--- db/migrate/20220308143945_create_build.rb
+++ db/migrate/20220308143945_create_build.rb
@@ -3,7 +3,7 @@ class CreateBuild < ActiveRecord::Migration[6.1]
     create_table :builds do |t|
       t.string :name
       t.integer :total_price
-      t.string :case
+      t.string :chassis
       t.string :cpu
       t.string :gpu
       t.string :motherboard
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,18 @@ puts "🌱 Seeding spices..."
 name = ["Awesome", "Crazy", "Fun", "Affordable"]
 part = ["Big 100", "Small 5000", "Shiny Part", "Newest Part", "Heavy Part"]
 10.times do 
-    Build.create(name: "#{name.sample} Build", total_price: rand(200..500), case: part.sample, cpu: part.sample, gpu: part.sample, motherboard: part.sample)
+    Build.create(name: "#{name.sample} Build", total_price: rand(200..500), chassis: part.sample, cpu: part.sample, gpu: part.sample, motherboard: part.sample)
 end
 
 # Parts (by Category)
 category = ["Case", "CPU", "GPU", "Memory", "Storage", "Power Supply", "Motherboard", "CPU Cooler"]
-20.times do
-    Part.create(name: "Placeholder", price: rand(50..200), category: category.sample, build_id: rand(1..10))
+10.times do
+    Part.create(name: "CPU Part", price: rand(50..200), category: category.sample, build_id: rand(1..10))
 end
 
+category = ["Case", "CPU", "GPU", "Memory", "Storage", "Power Supply", "Motherboard", "CPU Cooler"]
+10.times do
+    Part.create(name: "GPU Part", price: rand(50..200), category: category.sample, build_id: rand(1..10))
+end
 
 puts "✅ Done seeding!"
