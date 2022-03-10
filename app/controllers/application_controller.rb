691bea8fe60dcd3ab0cf8b77d3790279212dd575
Evan Beck <evandrydenbeck@gmail.com>
2022-03-10 15:11:16 -0500

    updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,27 @@ class ApplicationController < Sinatra::Base
     { message: "Hello!" }.to_json
   
   end
+
   get "/parts" do
      parts = Part.all
      parts.to_json
   end
+
   get "/builds" do
     builds = Build.all
     builds.to_json
   end
+
+  post "/builds" do
+    build = Build.create(
+      name: params[:name]
+      total_price: params[:total_price]
+      chassis: params[:chassis]
+      cpu: params[:cpu]
+      gpu: params[:gpu]
+      motherboard: params[:motherboard]
+    )
+    build.to_json
+  end
+
 end
